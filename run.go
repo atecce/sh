1d8d496f6fd6407cb75174a434879aa227ae47d2
atec <root@atec.pub>
2017-09-14 15:50:27 -0400

    handle exit error more properly

--- run.go
+++ run.go
@@ -1,20 +1,23 @@
 package sh
 
 import (
+	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/kr/pretty"
 )
 
 func Run(cmd *exec.Cmd) (string, *exec.ExitError) {
-
 	pretty.Logf("[INFO] running %s...", cmd.Args)
 	stdout, err := cmd.Output()
-	pretty.Log("[DEBUG] stdout:", string(stdout))
 	if err != nil {
-		pretty.Logln("[ERROR] command", cmd, "failed to run")
-		pretty.Logln("[INFO] attempting to return exit error, could panic here (TODO?)...")
-		return string(stdout), err.(*exec.ExitError)
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			pretty.Logln("[ERROR] stderr:", strings.Trim(string(exitErr.Stderr), "\n"))
+			return string(stdout), exitErr
+		}
+		pretty.Logln("[ERROR] command failed without exit error. quitting")
+		log.Fatal(err)
 	}
 	return string(stdout), nil
 }
